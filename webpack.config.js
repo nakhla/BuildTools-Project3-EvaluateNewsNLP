ea6e17eeccb80594e49cc7a0e1100c39e1477dcc
Magdi Nakhla <nakhla3@hotmail.com>
2021-12-08 20:14:56 +0200

    html-webpack-plugin@4 and clean-webpack-plugins installed and change the express server to serve dist folder instead of src/client and deleted link and script tags in views/index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path =  require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -10,5 +12,12 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
 }
