0360f8df0f8540c4f1f16f12feed3d6d1c9394df
Magdi Nakhla <nakhla3@hotmail.com>
2021-12-08 21:46:16 +0200

    style-loader@2 node-sass@4 css-loader@5 sass-loader@7 installed change extension to scss

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,10 @@
 import {handleSubmit} from './js/formHandler'
 
 console.log('hello magdi', {handleSubmit})
+
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+
--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,10 @@ module.exports = {
                 test: /\.js$/,
                 loader:'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
